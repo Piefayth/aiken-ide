0b5dff64260f6ac0db02c5cb506e2028eb6de62f
Piefayth <piefayth@gmail.com>
2024-03-22 22:33:36 -0500

    oops

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css'
 import { useAiken } from './hooks/useAiken';
 import { MonacoEditor } from './panels/editor/MonacoEditor';
